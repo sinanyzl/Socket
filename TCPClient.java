c4d08f5549cd1f71dbf605d1098eef0bb9cdae7c
Sinan Yozgatlı <75254489+sinanyzl@users.noreply.github.com>
2024-04-20 15:01:03 +0300

    Add files via upload

---
+++ TCPClient.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class TCPClient {
+  public static void main(String[] args) throws IOException {
+	  
+    Socket socket = new Socket();
+    socket.connect(new InetSocketAddress("127.0.0.1", 5001), 1000);
+    System.out.println("Connection Successful!");
+  }
+}
